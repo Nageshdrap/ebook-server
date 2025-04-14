12dd58d0d3316a1c617acf2f2a06dc9e26667f30
Nageshdrap <nageshachary2@gmail.com>
2025-04-14 13:44:26 +0530

    googleid model updated

--- models/User.js
+++ models/User.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     googleId:{
         type:String,
-        unique:true,
         default:null
     },
     uname :{
