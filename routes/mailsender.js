6f5b4bb34a2fcb8818ed0ed23b61463f11b26e4b
Nageshdrap <nageshachary2@gmail.com>
2025-05-25 08:05:25 +0530

    contact us

--- routes/mailsender.js
+++ routes/mailsender.js
@@ -44,6 +44,48 @@ router.post('/send-email', async(req,res)=>{
         })
     }
 
+});
+router.post('/send-contact', async(req,res)=>{
+    const { name , email , subject ,message } = req.body;
+
+    try {
+        const response = await axios.post('https://api.mailersend.com/v1/email',
+            {
+                from:{
+                    email:process.env.MAILERSEND_FROM_EMAIL,
+                    name:'Bookturn'
+                },
+                to:[
+                    {
+                        email:process.env.BUSINESS_OWNER_EMAIL,
+                        name:'business Owner'
+                    }
+                ],
+                subject:'Contact inquire from bookturn user',
+                text:`You received a new message:\n\nname: ${name}\ndepartment name: ${deptname}\nnumber: ${number}`,
+                html: `<h3>New Inquiry from Bookstore</h3>
+               <p><strong>Name:</strong> ${name}</p>
+               <p><strong>Email:</strong> ${email}</p>
+               <p><strong>Subject:</strong>${subject}</p>
+                <p><strong>Message:</strong>${message}</p>
+`
+            },
+            {
+                headers: {
+          Authorization: `Bearer ${process.env.MAILERSEND_API_KEY}`,
+          'Content-Type': 'application/json'
+            }
+        }
+        );
+        res.json({success:true,
+            message:'email sent successfully'
+        })
+    } catch (error) {
+        res.json({success:false,
+            message:'email send failled'
+        })
+    }
+
 })
 
 module.exports = router;
